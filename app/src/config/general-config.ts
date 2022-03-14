1d02fe69490ee25c1664b2390fdf462dddd53c24
Stefano Dalesandro <stefano.dalessandro@asamblo.com>
2022-03-14 02:46:18 -0300

    added config files

---
+++ app/src/config/general-config.ts
@@ -0,0 +1,6 @@
+// api url config
+export const API_URL = 'https://coding-challenge-api.aerolab.co/';
+
+// const config
+export const APP_NAME = 'AerolabShop';
+export const APP_DISPLAY_NAME = 'Aerolab Shop';
